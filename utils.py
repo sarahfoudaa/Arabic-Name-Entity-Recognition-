b53fb76d2674dab83c8f6dae46118c7eb69f8d09
Sarah Fouda <sfouda47@gmail.com>
2023-07-11 14:01:16 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,55 +2,115 @@
 #preprocesssing 
 
 import pandas as pd
+import pytz
+from datetime import datetime
 
-#for the test
-def sentence(test):
+def remove_punc(test):
+  ''' 
+  Function that removes punctuation from dataframe
+
+  Parameters:
+    test(DataFrame): dataframe of two columns ,words and labels, unprocessed and have punctuations in it
+    
+  Returns:
+    test(DataFrame):  dataframe of two columns ,words and labels, unprocessed and without punctuations in it
+  '''
   punc = '،-!"#$%&\'()*+,.-–/:;<=>?@[\ـ\]^_`{؛|}~«؟'
   punctuation = set(punc)
   # Create boolean mask to identify rows containing punctuation
   mask = test['words'].apply(lambda x: any(char in punctuation for char in x))
+  test = test[mask==False].reset_index(drop=True)
+  return test
 
-  # Filter original DataFrame using boolean mask
-  new_df = test[mask]
-  test_wo_punc = test[mask==False]
-  test_wo_punc = test_wo_punc.reset_index(drop=True)
-  test = test_wo_punc
+def preprocess_test(test):
+  ''' 
+  Function to process the test dataset.
+  The function removes the punctuation from the datframe, then concat the words into sentence(string) separat then with a ' ' corresponding to it their labels in  a list
 
+  Parameters:
+    test (DataFrame): A DataFrame of two columns, words(string) and labels(string), where each words has its label 
 
-  no_of_sen =0
-  df_sen = []
-  s = ''
-  entities_per_line = []
+  Returns:
+    sen_entities (DataFrame):A DataFrame of two columns, sentence(string) and list_entities(list of string) .
+   '''
+  test = remove_punc(test)
   entities = ['O', 'B-PERS', 'B-LOC', 'I-PERS', 'B-ORG', 'I-ORG', 'B-MISC', 'I-MISC', 'I-LOC']
-  ss = ''
-  df = pd.DataFrame(columns = ['labels'])
-  l = []
+  df_sen = [] #list of sentences
+  s = '' #sentence
+  df = pd.DataFrame(columns = ['labels'])#dataframe of lists of labels
+  l = []#list of labels
   for i in range (len(test)):
-    if test['words'][i]!='':
+    if test['words'][i]!='' and test['labels'][i] in entities:
       s = s +" "+ test['words'][i]
-      no_of_sen = no_of_sen+1
-      if test['labels'][i] in entities:
-        ss = ss +" "+ test['labels'][i]
-        l.append(test['labels'][i])
+      l.append(test['labels'][i])
     else:
-      df_sen.append(s)
+      df_sen.append(s[1:])
       s=''
-      entities_per_line.append(ss)
-      ss=''
-      #print("============",l)
       df.at[i, 'labels'] = l
       l=[]
   df_sen = pd.DataFrame(df_sen, columns=['sentence'])
-  entities_per_line = pd.DataFrame(entities_per_line, columns=['entities'])
-
   df = df.reset_index()
-  #print(df)
-  sen_entities = pd.DataFrame().assign(sentence=df_sen['sentence'], entities=entities_per_line['entities'],list_entities = df['labels'])
+  sen_entities = pd.DataFrame().assign(sentence=df_sen['sentence'],list_entities = df['labels'])
   return sen_entities
 
-  ''' sent flag in the parameters  
-  if test return ...
-  if train return ...
+#for train
+def preprocess_train(test):
+  ''' 
+  Function to process the train dataset.
+  The function removes the punctuation from the datframe, then collect the words into list corresponding to it their labels mapped to their IDs in  a list
+
+  Parameters:
+    test (DataFrame): A DataFrame of two columns, words(string) and labels(string), where each words has its label 
+
+  Returns:
+    sen_entities (DataFrame):A DataFrame of three columns,, sentence_id(int),sentence (list of strings) and list_entities(list of numbers) .
+   '''
+  test = remove_punc(test)
+  test['labels'] = test['labels'].replace(['B-LOC','B-MISC','B-ORG','B-PERS','I-LOC','I-MISC','I-ORG','I-PERS','O'], [0,1,2,3,4,5,6,7,8])
+  no_of_sen =0
+  df_sen =  pd.DataFrame(columns=['text'])#dataframe for lists of words
+  s = []#list of words
+  entities_per_line = pd.DataFrame(columns = ['labels'])#dataframe for lists of laels
+  ss = []#list of labels
+  for i in range (len(test)):
+    if test['words'][i]!='':
+      s.append(test['words'][i])
+      ss.append(test['labels'][i])
+    else:
+      df_sen.at[no_of_sen, 'text'] = s
+      s=[]
+      entities_per_line.at[no_of_sen, 'labels'] = ss
+      ss=[]
+      no_of_sen = no_of_sen+1
+  #sen_entities = pd.DataFrame().assign(sentence_id = df_sen['sentence'].index.astype(str) ,sentence=df_sen['sentence'], entities=entities_per_line['entities'])
+  sen_entities = pd.DataFrame().assign(sentence_id = df_sen['text'].index.astype(str) ,text=df_sen['text'], labels=entities_per_line['labels'])
+  return sen_entities
+
+def flatten(data):
+  ''' 
+  Function that flatten the list of lists of the labels to be 1Dim 
+  
+  Pararmeters:
+    data(DataFrame of lists): lists of the labels of each sentence
+
+  Returns:
+    flat(list): flatten list of the entites.
   '''
+  #irregular_list = data.tolist()
+  irregular_list = data
+  flatten_list = lambda irregular_list:[element for item in irregular_list for element in flatten_list(item)] if type(irregular_list) is list else [irregular_list]
+  flat =  flatten_list(irregular_list)
+ 
+  return flat
 
 
+def DT():
+  ''' 
+  Function that gers the date and timezone in Egypt
+
+  Returns:
+  egypt_time_str(string): A string that contains the date and time in Egypt
+   '''
+  egypt_tz = pytz.timezone('Egypt')
+  egypt_time = datetime.now(egypt_tz)
+  return egypt_time.strftime('D:%Y-%m-%dH-%H:%M:%S')
